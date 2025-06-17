5033f1f957587c7da3c9e96473d1f2dcf445e3af
Cha Varilla <104213101+ChaVarilla@users.noreply.github.com>
2025-06-17 21:40:54 +0800

    Add files via upload

---
+++ DataFilter.py
@@ -0,0 +1,33 @@
+
+
+class DataFilter:
+    def __init__(self):
+        self.SYNOPTIC_STATIONS = ['98134', '98223', '98232', '98325', '98328', '98336',
+                                 '98429', '98430', '98431', '98440', '98444', '98526',
+                                 '98531', '98536', '98543', '98618', '98630', '98646',
+                                 '98653', '98741', '98753', '98755', '98836', '98132',
+                                 '98133', '98222', '98233', '98324', '98327', '98334',
+                                 '98426', '98427', '98428', '98432', '98433', '98434',
+                                 '98435', '98446', '98538', '98546', '98548', '98553',
+                                 '98558', '98642', '98644', '98648', '98746', '98751',
+                                 '98752', '98851', '98329', '98332', '98422', '98425',
+                                 '98437', '98545', '98550', '98602', '98643', '98748']
+    def filter_synoptic_data(self):
+        with open("SMPH01 RPMM 150000.txt") as data:
+            for line in data:
+                if str(line[0:5]) in self.SYNOPTIC_STATIONS:
+                    with open("Synoptic Observations.txt", "a") as new_data:
+                        new_data.write(line)
+
+    def filter_with_cloud_group(self):
+        with open("Synoptic Observations.txt") as data:
+            for line in data:
+                data_per_station = line.split(" ")
+                index_guide = data_per_station.index("333")
+                print(index_guide)
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,15 @@
+from DataFilter import DataFilter
+
+data_filter_class = DataFilter()
+
+# data_filter_class.filter_synoptic_data()
+data_filter_class.filter_with_cloud_group()
+
+# data = "98134 32475 3//// 10286 20259 39914 40100 5//// 81275 333 20268 56999 59012 81819 83357"
+# list_data = data.split(" ")
+# index_guide = list_data.index("333")
+# print(index_guide)
+
+
+
+
